a0bcce814974fc6781de0cba5bd8039258c70bbf
dogukanyazar8 <37948900+dogukanyazar8@users.noreply.github.com>
2020-11-16 11:54:38 +0300

    #start

---
+++ ders_1.c
@@ -0,0 +1,28 @@
+/* 
+
+Ders1
+
+BIT             1 Bit
+NIBBLE          4 Bit
+BYTE            8 Bit
+WORD            2 Byte
+DOUBLE WORD     4 Byte
+QUAD WORD       8 Byte
+
+TIOBE INDEX
+
+C Programlama Dilinin Özellikleri
+
+Imperative              (Buyurgan)                 /\       Declarative                 (Bildirimsel)
+Procedural              (Prosedürel)                        (Programlama Paradigmaları) (C++ Multi-paradigm)
+Non-proprietary         (Mülkiyeti Yok)
+Middle-Level            (Orta Seviyeli)
+Static Typing           (Statik Tür Kavramı)
+General-purpose         (Genel Amaçlı)
+Efficient               (Verimli)
+Standart                (Standart)
+Artificial              (Yapay)
+Portable                (Taşınabilir)
+Expressive              (İfade Gücü Yüksek)
+
+ */
