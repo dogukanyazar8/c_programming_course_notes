37034708c54aaa718c10761130d11c9ec58824b5
dogukanyazar8 <dogukanyazar@gmail.com>
2021-03-04 23:00:53 +0300

    Ders15 Devam Ediyor

--- 015.c
+++ 015.c
@@ -35,7 +35,7 @@ Burada, bir döngü oluşturucaz ve bu döngünün kaç kez dönmesi gerektiği 
 
 #include <stdio.h>
 
-//eğer daha anlamlı bir isim mevcut değilse, değişkenin tek varlık nedeni fonksiyonun geri dönüş değerini oluşturmak ise, bu isimleri kullanabiliriz.
+//eğer daha anlamlı bir isim mevcut değilse, değişkenin tek varlık nedeni fonksiyonun geri dönüş değerini tutmak ise, bu isimleri kullanabiliriz.
 // result - ret
 int power(int base, int exp){
 
@@ -59,7 +59,86 @@ int main(){
 --------------------------------------------------------------------------
 
 Microsoft Visual Studio IDEsini kullanıyorsak, 
-Project -> project_name Properties -> Code Analysis -> Enable Microsoft Code Analysis
+Project -> project_name Properties -> Code Analysis -> Enable Microsoft Code Analysis [NO] 
+scanf gibi fonksiyonlarda uyarı vermemesi için.
+
+--------------------------------------------------------------------------
+
+sonsuz döngü - infinite loop
+
+sonsuz döngü terimi iki ayrı anlamda kullanılabiliyor.
+birincisi bir kodlama hatasını işaretlemek için kullanılıyor.
+
+int main(){
+    int low, high;
+    int n;
+    int i;
+
+    printf("Bir aralik girin: [low high]: ");
+    scanf("%d%d", &low, &high);
+    printf("Kaca tam bolunenler yazilsin: ");
+    scanf("%d", &n);
+
+    i = low;
+
+    while (i < high){
+        if (i % n == 0) {
+            printf("%d", i);
+            ++i;    //programcı burada i'nin arttırılma yerini yanlış yere yazmış.
+        }           //burada sonsuz bir döngü var.
+    }
+}
+
+//sonsuz döngü idiomu
+bir de öyle durumlar var ki, biz bilerek bir döngüden, döngünün kontrol ifadesinin yanlış olması nedeniyle döngüden çıksın istemiyoruz.
+
+while (1) {
+
+}
+
+Döngüden zaten çıkılmayacak. Program açık kaldığı sürece bu döngünün gövdesindeki deyimler yapılacak.
+Mesela embedded programlarının bir kısmı bu şekilde. Main fonksiyonları içinde böyle bir döngü deyimi var.
+
+Döngüden başka bir yolla çıkmak.
+Bir döngüden çıkış yolları:
+
+i)   döngünün kontrol ifadesinin yanlış olması ile
+ii)  break statement ile
+iii) return statement ile
+iv)  goto deyimi ile
+v)   programın sonlanması
+
+while (expr) {
+    //statement
+    //statement
+    if (c_ex)
+        return 1;
+    //statement
+    //statement
+
+while (expr) {
+    //statement
+    //statement
+    if (c_ex)
+        break;
+    //statement
+    //statement
+}
+
+while (expr) {
+    //statement
+    //statement
+    if (c_ex)
+        exit(EXIT_FAILURE);
+    //statement
+    //statement
+}
+
+Bu tarz programlama dillerinde standart kütüphanenin programları sonlandıran fonksiyon ya da fonksiyonları oluyor.
+exit
+abort
+
+27.00
 
 */
 
