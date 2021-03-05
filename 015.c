245ff116dcc7f182f879a7f7e4458a95e06162e3
dogukanyazar8 <dogukanyazar@gmail.com>
2021-03-05 15:23:19 +0300

    Ders15 Kurs Notları Devam Ediyor

--- 015.c
+++ 015.c
@@ -90,7 +90,7 @@ int main(){
 }
 
 //sonsuz döngü idiomu
-bir de öyle durumlar var ki, biz bilerek bir döngüden, döngünün kontrol ifadesinin yanlış olması nedeniyle döngüden çıksın istemiyoruz.
+bir de öyle durumlar var ki, biz bilerek bir döngüden, döngünün kontrol ifadesinin yanlış olması nedeniyle döngüden çıkılsın istemiyoruz.
 
 while (1) {
 
@@ -99,14 +99,18 @@ while (1) {
 Döngüden zaten çıkılmayacak. Program açık kaldığı sürece bu döngünün gövdesindeki deyimler yapılacak.
 Mesela embedded programlarının bir kısmı bu şekilde. Main fonksiyonları içinde böyle bir döngü deyimi var.
 
+Bu tarz programlama dillerinde standart kütüphanenin programları sonlandıran fonksiyon ya da fonksiyonları oluyor.
+exit
+abort
+
 Döngüden başka bir yolla çıkmak.
 Bir döngüden çıkış yolları:
 
 i)   döngünün kontrol ifadesinin yanlış olması ile
 ii)  break statement ile
 iii) return statement ile
-iv)  goto deyimi ile
-v)   programın sonlanması
+iv)  goto deyimi ile (goto deyiminin zaten varlık nedeni programın akışını koşulsuz belirli bir noktaya yönlendirmek. döngünün içinden döngünün dışındaki bir noktaya)
+v)   programın sonlanması (exit - abort -> bir fonksiyon çağrısı ile programın sonlandırılmasını sağlayıp, döngüden çıkabiliriz.)
 
 while (expr) {
     //statement
@@ -134,18 +138,193 @@ while (expr) {
     //statement
 }
 
-Bu tarz programlama dillerinde standart kütüphanenin programları sonlandıran fonksiyon ya da fonksiyonları oluyor.
-exit
-abort
+while döngü deyimi oluştururken döngünün kontrol ifadesini hemen yazmak istemek.
+a) bu döngünün başında belli olmayabilir. dinamik olarak döngünün gövdesinde belirli oluyor olabilir.
+b) evet böyle bir ifade yazılabilir ama böyle bir ifadeyi yazmak görece olarak zor olabilir.
 
-27.00
+Ama biz döngünün gövdesinde diğer yollardan biriyle döngüden çıkabiliriz.
+
+while (1) { //sonsuz döngü deyimi
+
+    if(????)
+}
+
+--------------------------------------------------------------------------
+
+Break Statement - Break Deyimi
+
+Break deyimi sentaks açısından C'nin en basit deyimlerinden biri.
+break bir anahtar sözcük. ve bu anahtar sözcüğü doğrudan sonlandırıcı atomu izliyor ve bu break deyimi oluyor.
+break;
+
+Break deyiminin geçerli olabilmesi için:
+
+a) bir döngü deyiminin gövdesinde,
+b) bir switch deyiminin gövdesinde,
+
+Eğer bu noktalardan birinde değilse, sentaks hatasıdır.
+
+Break deyiminin yürütülmesi programın akışını döngüden sonraki ilk deyime yönlendiriyor.
+
+while (expr) {
+    statement;
+    statement;
+    statement;
+    statement;
+    if (c_exp)  //eğer programın akışında, if ifadesi doğru olursa, break deyimi çalışır ve programın akışı döngüden sonraki ilk deyime yönlendirilir.
+        break;
+    statement;
+    statement;
+}
+    statement_after_loop;
+
+Bu döngüden, hangi noktada çıkmak istiyorsak, 
+
+while (1) {
+    if (!c_exp)  
+        break;
+    statement;
+    statement;
+    statement;
+    statement;
+    statement;
+    statement;
+}
+    statement_after_loop;
+
+Yukarıdaki döngü deyimi ile aşağıdaki döngü deyiminin hiçbir farkı yoktur.
+
+while (c_exp) {
+    statement;
+    statement;
+    statement;
+    statement;
+    statement;
+    statement;
+}
+    statement_after_loop;
+
+
+Örnek, aşağıdaki döngünün gövdesinde, if deyimindeki koşul ifadesi doğru olduğu sürece bu döngüdeki deyimler yapılacak.
+
+while (1) {
+    statement;
+    statement;
+    statement;
+    statement;
+    statement;
+    statement;
+    if (!c_exp)
+        break;
+}
+
+Bu da aslında do-while döngü deyiminin bir örneği. Döngünün koşul ifadesi döngünün başında kontrol edilmiyor, döngünün sonunda kontrol ediliyor.
+
+Döngüyü kontrol eden ifade döngünün başındaysa, bu normal while döngüsü.
+Döngüyü kontrol eden ifade döngünün sonundaysa, bu do while döngüsü.
+
+Bu IF - BREAK yapısı döngünün içerisinde sadece bir noktada değil, birden fazla noktada da kullanılabilir.
+
+--------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <ctype.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main(){
+    int c;
+    int ival;
+    int cnt = 0;
+    int sum = 0;
+    int min, max;
+
+    while (1) {
+        printf("Tam sayi girecek misiniz [e] [h] : ");
+        while ((c = getch()) != 'e' && c != 'h') //atama operatörünün ürettiği değer nesneye atanan değerdir. ayrıca kısa devre davranışı var. burada girişi e ve h tuşlarına kitledik.
+        ;                                        //null statement - döngünün gövdesinde bir şey yapılmayacak.
+        printf("%c\n", c);
+        if (c == 'h')
+            break;
+        printf("Tam sayiyi giriniz: ");
+        //scanf("%d", &ival);
+        ival = (rand() % 2 ? 1 : -1) * rand();
+        sum += ival;
+        if(cnt == 0)
+            min = max = ival;
+        else if (ival > max)
+            max = ival;
+        else if (ival < min)
+            min = ival;
+
+        ++cnt;
+        printf("%d\n [Giris Sayisi: %d]", ival, cnt);
+    }
+
+    if (cnt != 0){
+        printf("Toplam %d sayi girdiniz.\n", cnt);
+        printf("Ortalama = %f\n", (double)sum/cnt); //(double) tür dönüştürme operatörü. derleyic sum değişkenini double olarak işleme sokacak. 
+        printf("Max = %d\n", max);
+        printf("Min = %d\n", min);
+    }
+    else {
+        printf("Hic sayi girmediniz.\n");
+    }
+}
+
+--------------------------------------------------------------------------
+
+ 
 
 */
 
 #include <stdio.h>
+#include <ctype.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 int main(){
+    int c;
+    int ival;
+    int cnt = 0;
+    int sum = 0;
+    int max = INT_MIN; //Ön işlemci komutlarını kullanarak, max değişkenine olabilecek en küçük değeri verdik. makro konusuyla ilgili.
+    int min = INT_MAX; //Ön işlemci komutlarını kullanarak, max değişkenine olabilecek en büyük değeri verdik. makro konusuyla ilgili.
 
+    while (1) {
+        printf("Tam sayi girecek misiniz [e] [h] : ");
+        while ((c = getch()) != 'e' && c != 'h') //atama operatörünün ürettiği değer nesneye atanan değerdir. ayrıca kısa devre davranışı var. burada girişi e ve h tuşlarına kitledik.
+        ;                                        //null statement - döngünün gövdesinde bir şey yapılmayacak.
+        printf("%c\n", c);
+        if (c == 'h')
+            break;
+        printf("Tam sayiyi giriniz: ");
+        //scanf("%d", &ival);
+        ival = (rand() % 2 ? 1 : -1) * rand();
+
+        if(cnt == 0)
+            min = max = ival;
+        else if (ival > max)
+            max = ival;
+        else if (ival < min)
+            min = ival;
+
+        sum += ival;
+        ++cnt;
+
+        printf("%d\n [Giris Sayisi: %d]", ival, cnt);
+    }
+
+    if (cnt != 0){
+        printf("Toplam %d sayi girdiniz.\n", cnt);
+        printf("Ortalama = %f\n", (double)sum/cnt); //(double) tür dönüştürme operatörü. derleyic sum değişkenini double olarak işleme sokacak. 
+        printf("Max = %d\n", max);
+        printf("Min = %d\n", min);
+    }
+    else {
+        printf("Hic sayi girmediniz.\n");
+    }
 }
 
 
